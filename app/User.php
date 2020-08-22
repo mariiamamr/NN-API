1ba9ab346cbf19fb1a9e50410090fc5df53cc698
aya-mahmoud-99 <ayamahmoudabdelfatah99@gmail.com>
2020-08-23 00:18:19 +0200

    2ay 7aga

--- app/User.php
+++ app/User.php
@@ -30,7 +30,7 @@ public function profile()
         return $this->hasOne(UserInfo::class, 'user_id');
        /// return 1;
     }
-    public function uni_degress()
+public function uni_degress()
     {
         return $this->belongsTo(UniversityDegree::class);
     }
