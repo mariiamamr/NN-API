2be84d471a2ed763910f8105dae91439ebab9c29
Aya Elsackaan <ayaelsackaan.1999@gmail.com>
2020-08-25 23:58:22 +0200

    validations

--- app/Http/Controllers/CreateSessionController.php
+++ app/Http/Controllers/CreateSessionController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller; 
+use App\User; 
+use App\UserInfo; 
+use App\UniversityDegree;
+use Validator;
+
 
 class CreateSessionController extends Controller
 {
-    //
+    public function createSession(Request $request){
+        $request->validate([
+            'time_from' => 'required',
+            'date' => 'required',
+            'weekly' => 'boolean',
+        ]);
+        if ($request->date == date('Y-m-d')) {
+            $should_start = \Carbon\Carbon::parse('now')->addHours(2);
+            if ($request->time_from <= $should_start->format('H:i')) {
+                return response()->json(['error' => "less than two hours left"], 403);   
+            }
+          }
+//
+        return response()->json(['message'=>"session created"], 200); 
+    
+    
+    
+    }
+
 }
