c3579086f4d78789b7015bff9d60c872b28365fb
mariiamamr <mariam.amr.abdelnasser@gmail.com>
2020-09-01 16:28:49 +0200

    report session modifications

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Container\Contracts\Reports\ReportContract;
 
 class ReportController extends Controller
 {
+
+    protected $report;
+  
+    public function __construct( ReportContract $report)
+      {
+        $this->report = $report;
+      }
 
     public function report(Request $request)
     {
